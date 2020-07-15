7bd62467f6ea91be2b82a8c93134ece724fb9e88
Joel <61564883+jvarnadore@users.noreply.github.com>
2020-07-15 00:26:13 -0400

    Add files via upload

--- Nested-Auto-Zip.ps1
+++ Nested-Auto-Zip.ps1
@@ -9,23 +9,31 @@ And then repeat.
    1) List of the folderes we want to repeat this job for.
    2) A script that will go into $folder, go into the messages subdirectory, and then compress all of the .emls in the folder into a zip called $folder
 #>
-
+#Script would start with using -Path on the Year folder and getting a list of folders named after months.
 $MonthFolderList = Get-ChildItem -Path "\\tn-web\exported email\Testing";
 
+#A double foreach nest to get into each day's folder inside each month.
 foreach ($MonthFolder in $MonthFolderList) {
    $DayFolderList = Get-ChildItem -Path $MonthFolder.FullName;
    foreach ($DayFolder in $DayFolderList) {
+       #Turning $DayFolder.FullName into a variable we can use for naming.
       $DayFolderFullName = $DayFolder.FullName;
+      #Each Day folder holdes it's EMLs in a nested folder, so we append it here and designate what we're looking for.
       $DayFolderPath = $DayFolderFullName += "\messages\*.eml";
+      #split up the complicated naming formule based on the numerous _ in the name.
       $ArchiveSplitName = $DayFolder.Name -split "_";
+      #And IF/Else statement to filter out whether or not the archive should included _spam at the end.
       if ($ArchiveSplitName[2] -eq "spam"){
          $ArchiveName = $ArchiveSplitName[0] + "_" +  $ArchiveSplitName[1] + "_" + $ArchiveSplitName[2];
       }
       Else {
          $ArchiveName = $ArchiveSplitName[0] + "_" +  $ArchiveSplitName[1];
       }
+      #Creating the variable used for our Compress-Archive path
       $DestinationPath = $DayFolder.FullName + "\messages\" + $ArchiveName;
+      #Executing the compress command.
       Compress-Archive -path $DayFolderPath -DestinationPath $DestinationPath -CompressionLevel Fastest;
+      #notification of the completion of each archive.
       Write-Host $ArchiveName "has been created.";
    }
 }
